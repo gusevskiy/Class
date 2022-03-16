865d313396779d95fe0d1bae73539ad68c7002be
vladimir <vlgu@mail.ru>
2022-03-16 18:34:58 +0300

    str 74

--- writing constructor code/person.py
+++ writing constructor code/person.py
@@ -16,6 +16,9 @@ class Person:
 
 
 class Manager(Person):
+    def __init__(self, name, pay): # переопределить конструктор
+        Person.__init__(self, name, 'mgr', pay) # выполнять исходный с mgr
+
     def giveRaise(self, percent, bonus=.10):
         Person.giveRaise(self, percent + bonus)
 
@@ -24,18 +27,15 @@ class Manager(Person):
 
 if __name__ == '__main__':
     bob = Person('Bob Smith')
-    sue = Person('Sue Jones', job = 'dev', pay=100000)
+    sue = Person('Sue Jones', job='dev', pay=100000)
     print(bob)
     print(sue)
     print(bob.lastName(), sue.lastName())
     sue.giveRaise(.10)
     print(sue)
-    tom = Manager('Tom Jones', 'mgr', 50000)
+    tom = Manager('Tom Jones', 50000)
 #Manager: __init__ # возможно это в новой версии python ненужно
     tom.giveRaise(.10)
     print(tom.lastName())
     print(tom)
-    print('--ALL three--')
-    for obj in (bob, sue, tom):
-        obj.giveRaise(.10)
-        print(obj)
+
