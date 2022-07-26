ed10243162f66347f96f44e45f619a8a4f8009a7
gusevskiy <vlgu@mail.ru>
2022-07-26 11:37:49 +0300

    стр 96 продолжаем) Детали реализации классов

--- writing constructor code/makedb.py
+++ writing constructor code/makedb.py
@@ -1,14 +1,29 @@
 from person import Person, Manager
+import shelve
+import glob
 
 
 bob = Person('Bob Smith')
 sue = Person('Sue Jones', job='dev', pay=100000)
 tom = Manager('Tom Jones', 50000)
 
-import shelve
-
 
 db = shelve.open('persondb')
 for obj in (bob, sue, tom):
     db[obj.name] = obj
 db.close()
+
+
+if __name__ == '__main__':
+    print(glob.glob('person*'))
+    db = shelve.open('persondb')
+    print(len(db))
+    print(list(db.keys()))
+    bob = db['Bob Smith']
+    print(bob)
+    print(bob.lastName())
+    for key in db:
+        print(key, '=>', db[key])
+    print('sorted')
+    for key in sorted(db):
+        print(key, '=>', db[key])
